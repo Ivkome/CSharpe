d33ee48448febf76a2acafbe16619638361c69ee
Ivan Steve Kouemo <ivan_kouemo@edus4080.hs-esslingen.de>
2017-10-11 19:13:06 +0200

    add first project

---
+++ SecondStep/SecondStep/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SecondStep
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
