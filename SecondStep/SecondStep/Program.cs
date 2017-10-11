15b28741989c377041c8010ab6e67e8864d1ca28
Ivan Steve Kouemo <ivan_kouemo@edus4080.hs-esslingen.de>
2017-10-11 19:18:38 +0200

    test commit

--- SecondStep/SecondStep/Program.cs
+++ SecondStep/SecondStep/Program.cs
@@ -7,6 +7,7 @@ namespace SecondStep
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("test commit");
         }
     }
 }
